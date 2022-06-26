49ad9b956286a00fa7769a984b544eb2a40631b9
kermie123 <rcheng1729@gmail.com>
2022-06-25 20:17:23 -0700

    used numpy to theoretically speed up computations

--- algo.py
+++ algo.py
@@ -11,11 +11,18 @@ where F_ij is the force pointing from i from overlapping neighbor j, i.e,
 where c is a force constant, d_ij is the center-center distance bwtween hypersphere i and j
     (4c) move the hypersphere i with displacement dx = k*F_total (Note dx is a vector)
 (5) repeat steps in (4) until the packing is overlapping free up to a tolerance
+
+
+Test on higher dimensions
+Fix the contact list problem
+Partition the space in halfs (new idea)?
+Check time complexity
 '''
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import truncnorm
-import copy
 import math
 from scipy.special import gamma
 
@@ -26,121 +33,145 @@ def get_truncated_normal(mean, sd, low, upp):
 
 class Sphere:
 
-    radius = 1
-
-    def __init__(self, dimension, center_pos):
+    def __init__(self, dimension, center_pos, radius=None, neighbor_list=None):
         self.dimension = dimension
         self.center_pos = center_pos
+        # might want to change this list later
+        self.neighbor_list = neighbor_list if neighbor_list is not None else []
+        self.radius = radius if radius is not None else 1
+
+    def clear_neighbor_list(self):
+        self.neighbor_list = []
 
 
 class Packing:
 
-    c = .5  # force constant
-    k = 1  # displacement constant?
-    change = 1.0001  # isotropically resize constant
+    c = .4  # force constant
+    change = 1.00001  # isotropically resize constant
+    radius = 1
 
-    def __init__(self, dimension, numspheres, l, spheres=None, relaxed=None, contact=None):
+    def __init__(self, dimension, numspheres, length, spheres=None, relaxed=None):
         self.dimension = dimension
         self.numspheres = numspheres
-        self.l = l
-        self.spheres = spheres if spheres is not None else []
+        self.length = length
+        self.spheres = spheres if spheres is not None else np.empty(
+            numspheres, dtype=object)
         self.relaxed = relaxed if relaxed is not None else False
-        self.contact = contact if contact is not None else []
 
-    # only usable in two dimensions
+    # draws the packing in 2 dimensions, method is good
     def draw(self):
         plt.axes()
-        boundary = plt.Rectangle((-self.l/2, -self.l/2), self.l, self.l, fc='None', ec='black')
+        boundary = plt.Rectangle((-self.length/2, -self.length/2),
+                                 self.length, self.length, fc='None', ec='black')
         plt.gca().add_patch(boundary)
         plt.axis('scaled')
         for i in range(len(self.spheres)):
             circle = plt.Circle((self.spheres[i].center_pos[0], self.spheres[i].center_pos[1]),
-                                Sphere.radius, fc='None', ec='black')
+                                self.radius, fc='None', ec='black')
             plt.gca().add_patch(circle)
-            point = plt.plot(self.spheres[i].center_pos[0], self.spheres[i].center_pos[1], 'bo')
+            plt.plot(self.spheres[i].center_pos[0],
+                     self.spheres[i].center_pos[1], 'bo')
         plt.show()
 
     def initial_fill(self):
+        # self.radius = 1.5/2*(self.length/(self.numspheres**(1/self.dimension)))
+        self.radius = 1
         for i in range(self.numspheres):
-            x = get_truncated_normal(0, self.l/4, -self.l/2, self.l/2)
+            x = get_truncated_normal(
+                0, self.length/4, -self.length/2, self.length/2)
             center = x.rvs(size=self.dimension)
-            self.spheres.append(Sphere(self.dimension, np.array(center)))
+            self.spheres[i] = Sphere(
+                self.dimension, np.array(center), self.radius)
 
+    # might be able to find a more efficient way to reuse the previous neighbor_list
     def find_contact(self):
         self.relaxed = True
-        self.contact = np.zeros((self.numspheres, self.numspheres))
         for i in range(self.numspheres):
+            self.spheres[i].clear_neighbor_list()
             for j in range(i+1, self.numspheres):
-                diff = np.subtract(self.spheres[i].center_pos, self.spheres[j].center_pos)
-                if np.linalg.norm(diff) < 2*Sphere.radius:
-                    self.contact[i][j] = 1
+                diff = np.subtract(
+                    self.spheres[i].center_pos, self.spheres[j].center_pos)
+                if np.linalg.norm(diff) < 2*self.radius:
+                    self.spheres[i].neighbor_list.append(np.array([j, 0]))
                     self.relaxed = False
                 # kind of an approximate method, maybe go back and look at it with math later
-                elif np.linalg.norm(diff) > self.l - 2*Sphere.radius:
-                    newcenter1 = copy.deepcopy(self.spheres[i].center_pos)
-                    newcenter2 = copy.deepcopy(self.spheres[j].center_pos)
-                    for k in range(self.dimension):
-                        diff = np.subtract(newcenter1, newcenter2)
-                        if abs(diff[k]) > self.l - 2*Sphere.radius:
-                            if self.spheres[i].center_pos[k] > self.l/2 - Sphere.radius:
-                                newcenter1[k] = newcenter1[k] - self.l
-                            elif self.spheres[i].center_pos[k] < -self.l/2 + Sphere.radius:
-                                newcenter1[k] = newcenter1[k] + self.l
-                            elif self.spheres[j].center_pos[k] > self.l/2 - Sphere.radius:
-                                newcenter2[k] = newcenter2[k] - self.l
-                            elif self.spheres[j].center_pos[k] < -self.l/2 + Sphere.radius:
-                                newcenter2[k] = newcenter2[k] + self.l
+                elif np.linalg.norm(diff) > self.length - 2*self.radius:
+                    newcenter1 = self.spheres[i].center_pos.copy()
+                    newcenter2 = self.spheres[j].center_pos.copy()
                     diff = np.subtract(newcenter1, newcenter2)
-                    if np.linalg.norm(diff) < 2*Sphere.radius:
-                        self.contact[i][j] = 2
+                    for k in range(self.dimension):
+                        if abs(diff[k]) > self.length - 2*self.radius:
+                            if self.spheres[i].center_pos[k] > self.length/2 - self.radius:
+                                newcenter1[k] = newcenter1[k] - self.length
+                            elif self.spheres[i].center_pos[k] < -self.length/2 + self.radius:
+                                newcenter1[k] = newcenter1[k] + self.length
+                            elif self.spheres[j].center_pos[k] > self.length/2 - self.radius:
+                                newcenter2[k] = newcenter2[k] - self.length
+                            elif self.spheres[j].center_pos[k] < -self.length/2 + self.radius:
+                                newcenter2[k] = newcenter2[k] + self.length
+                    diff = np.subtract(newcenter1, newcenter2)
+                    if np.linalg.norm(diff) < 2*self.radius:
+                        self.spheres[i].neighbor_list.append(np.array([j, 1]))
                         self.relaxed = False
 
     def relax_once(self):
+
         # isotropically rescale
-        self.l = Packing.change * self.l
+        self.length = Packing.change * self.length
         for i in range(self.numspheres):
-            self.spheres[i].center_pos = np.multiply(self.spheres[i].center_pos, Packing.change)
+            self.spheres[i].center_pos = self.spheres[i].center_pos * \
+                Packing.change
 
         # displace with forces
-        sum = np.array(np.zeros((self.numspheres, self.dimension)))
-        for i in range(self.numspheres):
-            for j in range(i+1, self.numspheres):
-                if self.contact[i][j] == 1:  # represents direct contact between spheres
-                    jtoi = np.subtract(self.spheres[i].center_pos, self.spheres[j].center_pos)
+        sum = np.zeros((self.numspheres, self.dimension))
+        for iindex in range(self.numspheres):
+            for jobj in self.spheres[iindex].neighbor_list:
+                j = self.spheres[jobj[0]]
+                jindex = jobj[0]
+                jkey = jobj[1]
+                i = self.spheres[iindex]
+                # represents direct contact between spheres
+                if jkey == 0:
+                    jtoi = np.subtract(
+                        i.center_pos, j.center_pos)  # represents i - j
                     norm = np.linalg.norm(jtoi)
                     force = max(
-                        0, (2 * Sphere.radius - norm) * Packing.c)
-                    sum[i] += force * np.array(jtoi) / norm
-                    sum[j] += -1 * force * np.array(jtoi) / norm
-                elif self.contact[i][j] == 2:  # represents wrapped contact between spheres
-                    newcenter1 = copy.deepcopy(self.spheres[i].center_pos)
-                    newcenter2 = copy.deepcopy(self.spheres[j].center_pos)
+                        0, (2 * self.radius - norm) * Packing.c)
+                    sum[iindex, :] = sum[iindex, :] + force * jtoi / norm
+                    sum[jindex, :] = sum[iindex, :] + -1 * force * jtoi / norm
+
+                # represents wrapped contact between spheres
+                elif jkey == 1:
+                    newcenter1 = i.center_pos.copy()
+                    newcenter2 = j.center_pos.copy()
+                    diff = np.subtract(newcenter1, newcenter2)
                     for k in range(self.dimension):
-                        diff = np.subtract(newcenter1, newcenter2)
-                        if abs(diff[k]) > self.l - 2*Sphere.radius:
-                            if self.spheres[i].center_pos[k] > self.l/2 - Sphere.radius:
-                                newcenter1[k] = newcenter1[k] - self.l
-                            elif self.spheres[i].center_pos[k] < -self.l/2 + Sphere.radius:
-                                newcenter1[k] = newcenter1[k] + self.l
-                            elif self.spheres[j].center_pos[k] > self.l/2 - Sphere.radius:
-                                newcenter2[k] = newcenter2[k] - self.l
-                            elif self.spheres[j].center_pos[k] < -self.l/2 + Sphere.radius:
-                                newcenter2[k] = newcenter2[k] + self.l
+                        if abs(diff[k]) > self.length - 2*self.radius:
+                            if i.center_pos[k] > self.length/2 - self.radius:
+                                newcenter1[k] = newcenter1[k] - self.length
+                            elif i.center_pos[k] < -self.length/2 + self.radius:
+                                newcenter1[k] = newcenter1[k] + self.length
+                            elif j.center_pos[k] > self.length/2 - self.radius:
+                                newcenter2[k] = newcenter2[k] - self.length
+                            elif j.center_pos[k] < -self.length/2 + self.radius:
+                                newcenter2[k] = newcenter2[k] + self.length
 
                     twotoone = np.subtract(newcenter1, newcenter2)
+                    norm = np.linalg.norm(twotoone)
                     force = max(
-                        0, (2 * Sphere.radius - np.linalg.norm(twotoone)) * Packing.c)
-                    sum[i] += force * np.array(twotoone) / np.linalg.norm(twotoone)
-                    sum[j] += -1 * force * np.array(twotoone) / np.linalg.norm(twotoone)
+                        0, (2 * self.radius - norm) * Packing.c)
+                    sum[iindex, :] = sum[iindex, :] + force * twotoone / norm
+                    sum[jindex, :] = sum[iindex, :] + - \
+                        1 * force * twotoone / norm
 
         # apply the displacement and wrap locations
         for i in range(self.numspheres):
             for j in range(self.dimension):
-                newcenter = self.spheres[i].center_pos[j] + sum[i][j]
-                while newcenter > self.l/2:
-                    newcenter -= self.l
-                while newcenter < -self.l/2:
-                    newcenter += self.l
+                newcenter = self.spheres[i].center_pos[j] + sum[i, j]
+                while newcenter > self.length/2:
+                    newcenter -= self.length
+                while newcenter < -self.length/2:
+                    newcenter += self.length
                 self.spheres[i].center_pos[j] = newcenter
 
     def relax(self):  # improve the number of steps between checking neighbors
@@ -151,12 +182,12 @@ class Packing:
             i += 1
             if i % 3 == 0:
                 self.find_contact()
-        self.write_results()
 
     def write_results(self):
-        singlevolume = (math.pi ** (self.dimension/2)) * (Sphere.radius **
+        singlevolume = (math.pi ** (self.dimension/2)) * (self.radius **
                                                           self.dimension) / gamma(self.dimension/2 + 1)
-        results = str(singlevolume * self.numspheres / (self.l ** self.dimension)) + '\n'
+        results = str(singlevolume * self.numspheres /
+                      (self.length ** self.dimension)) + '\n'
         for i in self.spheres:
             results += str(i.center_pos) + '\n'
         file = open("output.txt", "w+")
@@ -165,9 +196,12 @@ class Packing:
 
 
 def main():
-    p = Packing(2, 20, 7)
+    # dim = 2
+    # p = Packing(dim, 10**dim, 1)
+    p = Packing(2, 16, 7)
     p.initial_fill()
     p.relax()
+    p.draw()
 
 
 if __name__ == '__main__':
